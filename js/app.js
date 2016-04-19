1a18cbd0fd472c8af13dfea78c40bc439bbde3d3
LukMon <monikaa.lukowska@gmail.com>
2016-04-19 16:43:21 +0200

    ruszające się - nie ruszające chmurki

--- js/app.js
+++ js/app.js
@@ -1,5 +1,26 @@
 $(function(){
 
-  console.log("Działa!");
+ function parallaxClouds(){
+
+
+   var clouds = $(".cloud-parallax");
+
+  //  console.log (wScroll);
+   console.log(clouds);
+
+
+   $(window).scroll(function(){
+var wScroll = $(this).scrollTop();
+     clouds.css({
+       "transform" : "translate(0px, "+ wScroll /10 +"%)"
+     });
+
+     console.log(clouds.css)
+
+   });
+
+ }
+
+ parallaxClouds()
 
 });
