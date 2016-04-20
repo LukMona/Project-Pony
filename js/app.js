9587168c4eda2f50e24d059b27d43e1c4cd0ee15
LukMon <monikaa.lukowska@gmail.com>
2016-04-20 11:30:08 +0200

    working pony slider

--- js/app.js
+++ js/app.js
@@ -1,83 +1,84 @@
 $(function(){
 
- function parallaxClouds(){
+  function parallaxClouds(){
 
-   var clouds = $(".cloud-parallax");
+    var clouds = $(".cloud-parallax");
 
-   $(window).scroll(function(){
-     var wScroll = $(this).scrollTop();
-     clouds.css({
-       "transform" : "translate(0px, "+ wScroll/2 +"px)"
-     });
-   });
- };
+    $(window).scroll(function(){
+      var wScroll = $(this).scrollTop();
+      clouds.css({
+        "transform" : "translate(0px, "+ wScroll/2 +"px)"
+      });
+    });
+  };
 
- parallaxClouds();
+  parallaxClouds();
 
 
- function infinitePonySlider(){
-   var next = $("#nextPony");
-   var prev = $("#prevPony");
-   var list = $(".pony-list");
-   var index = 0;
-   var widthPhoto = $("li").first().width();
+  function infinitePonySlider(){
+    var next = $("#nextPony");
+    var prev = $("#prevPony");
+    var list = $(".pony-list").find("li");
+    var widthPhoto = $("li").first().width();
 
-   var ul = $(".pony-list");
-   var firstElement = list.first().clone();
-   ul.append(firstElement);
-   var lastElement = list.last().clone();
-   ul.prepend(lastElement);
+    var ul = $(".pony-list");
+    var firstElement = list.first().clone();
+    ul.append(firstElement);
+    var lastElement = list.last().clone();
+    ul.prepend(lastElement);
 
-   var list = $(".pony-list");
+    var list = $(".pony-list").find("li");
 
-   function setUlSize(){
-     list.parent().css("width", (widthPhoto * list.length));
-   };
+    function setUlSize(){
+      list.parent().css("width", (widthPhoto * list.length));
+    };
 
-   setUlSize();
- //
- //   function setFirstPhoto(){
- //     list.parent().css("left", -widthPhoto);
- //   };
- //
- //   setFirstPhoto();
- //
- //   next.on("click", function(){
- //     console.log("Działa!");
- //     index++;
- //
- //     ul.animate({
- //       left: -(widthPhoto*index)
- //     }, {
- //       duration: 800,
- //       complete: function moveToFirst(){
- //         if (index === list.length-1){
- //           index = 1;
- //           list.parent().css("left", -widthPhoto);
- //         };
- //       }
- //     });
- //   });
- //
- //   prev.on("click", function(){
- //     console.log("Działa!");
- //     index--;
- //
- //     ul.animate({
- //       left: -(widthPhoto*index)
- //     }, {
- //       duration: 800,
- //       complete: function moveToLast(){
- //         if (index === 0){
- //           index = list.length-2;
- //           list.parent().css("left", -widthPhoto*(list.length-2));
- //         };
- //       }
- //     });
- //   });
- //
- };
+    setUlSize();
 
- infinitePonySlider();
+      function setFirstPhoto(){
+        list.parent().css("left", -widthPhoto);
+      };
+
+      setFirstPhoto();
+
+      var index = 1;
+
+      next.on("click", function(){
+        console.log("Działa!");
+        index++;
+
+        ul.animate({
+          left: -(widthPhoto*index)
+        }, {
+          duration: 800,
+          complete: function moveToFirst(){
+            if (index === list.length-1){
+              index = 1;
+              list.parent().css("left", -widthPhoto);
+            };
+          }
+        });
+      });
+
+      prev.on("click", function(){
+        console.log("Działa!");
+        index--;
+
+        ul.animate({
+          left: -(widthPhoto*index)
+        }, {
+          duration: 800,
+          complete: function moveToLast(){
+            if (index === 0){
+              index = list.length-2;
+              list.parent().css("left", -widthPhoto*(list.length-2));
+            };
+          }
+        });
+      });
+
+  };
+
+  infinitePonySlider();
 
 });
